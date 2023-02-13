4b3b9cac1b11bf289c9852f6beb4c62a295f57df
Tall-Al <Alcroi.1255@gmail.com>
2023-02-13 09:35:32 -0800

    added in our addToCart feature

--- src/components/cart-dropdown/cart-dropdown.components.jsx
+++ src/components/cart-dropdown/cart-dropdown.components.jsx
@@ -1,11 +1,22 @@
 import './cart-dropdown.styles.scss';
+
+import { CartContext } from '../../contexts/cart-context';
+
+import { useContext } from 'react';
 import Button from '../button/button.component'
+import CartItem from '../cart-item/cart-item.component'
 
 const CartDropdown = () => {
+    const { cartItems } = useContext(CartContext);
+
+
     return(
         <div className='cart-dropdown-container'>
-            <div className='cart-items'/>
+            {/* This div was initially self closing but I adjusted it against the course because the cart items were not displaying correctly */}
+            <div className='cart-items'> 
+            {cartItems.map((item) => <CartItem key={item.id} cartItem={item}/>)}
             <Button > GO TO CHECKOUT </Button>
+            </div>
         </div>
     )
 }
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,16 +4,20 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import { CartContext } from '../../contexts/cart-context';
 
+import CartItem from '../cart-item/cart-item.component';
+
 import './cart-icon.styles.scss';
 
-const CartIcon = () => {
-    const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+const CartIcon = (cartItem) => {
+    const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
     const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+
+    const { quantity } = cartItem;
     
     return(
         <div className='cart-icon-container' onClick={toggleIsCartOpen}>
             <ShoppingIcon className='shopping-icon' />
-            <span className='item-count'>10</span>
+            <span className='item-count'>{cartCount}</span>
         </div>
     );
 };
